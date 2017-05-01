e3e9e768e50f8b8e9c625c5a15419f20d9266ec3
test user <test@git.com>
2017-05-01 11:09:07 +0200

    B2 C5 commit

--- Test.java
+++ Test.java
@@ -4,7 +4,7 @@ public class Test {
 		System.out.println("Hello GitHub");
 		
 		System.out.println("Hello GitHub 3 or 4");
-		
+		System.out.println("Hello GitHub B2 before merge");
 		System.out.println("Hello GitHub Branch 2");
 		
 	}
